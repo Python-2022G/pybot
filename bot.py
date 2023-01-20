7cd7e9196d69fc6d890ca12115efd6df1c7945d4
djumanov <diyorbekjumanov1120@gmail.com>
2023-01-20 17:06:41 +0500

    text

--- bot.py
+++ bot.py
@@ -5,5 +5,7 @@ TOKEN = os.environ['TOKEN']
 
 bot = telegram.Bot(token=TOKEN)
 
-user = bot.getMe()
-print(user.first_name)
+updates = bot.getUpdates()
+last_update = updates[-1]
+
+print(last_update.message.text)
