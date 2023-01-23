9d9cf0e1990c16da86d6f399a13aad3684768dac
djumanov <diyorbekjumanov1120@gmail.com>
2023-01-23 17:53:33 +0500

    dog and cat

--- bot.py
+++ bot.py
@@ -12,6 +12,12 @@ def start(update: Update, context: CallbackContext):
 def help(update: Update, context: CallbackContext):
     update.message.reply_text('how can i help you?')
 
+def cat(update: Update, context: CallbackContext):
+    response = requests.get('https://aws.random.cat/meow')
+    url = response.json()['file']
+
+    update.message.reply_photo(url)
+
 def dog(update: Update, context: CallbackContext):
     response = requests.get('https://random.dog/woof.json')
     url = response.json()['url']
@@ -32,6 +38,7 @@ def main():
 
     # message handler 
     dispatcher.add_handler(handler=MessageHandler(filters=filters.Filters.text('dog'), callback=dog))
+    dispatcher.add_handler(handler=MessageHandler(filters=filters.Filters.text('cat'), callback=cat))
 
     updater.start_polling()
     updater.idle()
