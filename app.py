e25b9717fe4f09a46f79ecb0a8776711bb89289c
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-13 12:13:52 +0500

    webhook

---
+++ app.py
@@ -0,0 +1,47 @@
+# deploy bot with flask
+from flask import Flask, jsonify, request
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from bot import start, help, dog, cat
+from telegram import Update, Bot
+import os
+
+# run ngrok
+# ngrok http 5000
+
+TOKEN = os.environ['TOKEN']
+
+bot = Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.set_webhook('https://8eef-84-54-76-136.in.ngrok.io/api/')
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/api/', methods=['POST'])
+def api():
+    print('ok')
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher = Dispatcher(bot, None, workers=0)
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(MessageHandler(Filters.regex('dog'), dog))
+    dispatcher.add_handler(MessageHandler(Filters.regex('cat'), cat))
+
+    dispatcher.process_update(update)
+    return jsonify({'ok': True})
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+        
